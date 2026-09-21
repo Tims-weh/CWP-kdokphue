75b6e40a4de2396bf91015f0cdf396b82d1babcd
Tims-weh <68010050@kmitl.ac.th>
2026-09-21 18:44:11 +0700

    Add files via upload

---
+++ discovery_piscine/cell04/ex01/age.py
@@ -0,0 +1,5 @@
+inp = int(input('Please tell me your age: '))
+print(f'You are currently {inp} years old.')
+print(f"In 10 years, you'll be {inp + 10} years old.")
+print(f"In 20 years, you'll be {inp + 20} years old.")
+print(f"In 30 years, you'll be {inp + 30} years old.")
