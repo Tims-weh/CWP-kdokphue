bf1dc46db0cc1228207b2de474903a1aba187b18
Tims-weh <68010050@kmitl.ac.th>
2026-09-22 21:59:37 +0700

    Update parameters.py

--- discovery_piscine/cell05/ex04/parameters.py
+++ discovery_piscine/cell05/ex04/parameters.py
@@ -1,3 +1,4 @@
-many_words = input()
-word = [str(i.strip('"')) for i in many_words.split('" "')]
-print("Number of parameters:", len(word) if word[0] else 0)
+import sys
+
+num_params = len(sys.argv) - 1
+print(f"Number of parameters: {num_params}.")
