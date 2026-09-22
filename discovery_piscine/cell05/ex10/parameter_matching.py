b0d9cec4512859518e9c6f77226c5694bef8e660
Tims-weh <68010050@kmitl.ac.th>
2026-09-22 21:57:37 +0700

    Update parameter_matching.py

--- discovery_piscine/cell05/ex10/parameter_matching.py
+++ discovery_piscine/cell05/ex10/parameter_matching.py
@@ -1,9 +1,10 @@
-word = input().strip('"')
-if not word:
-    print("none")
-else:
-    text_input = str(input("What was the parameter? "))
-    if word == text_input:
+import sys
+
+if len(sys.argv) == 2:
+    text_input = input("What was the parameter? ")
+    if sys.argv[1] == text_input:
         print("Good job!")
     else:
-            print("Nope, sorry...")
+        print("Nope, sorry...")
+else:
+    print("none")
